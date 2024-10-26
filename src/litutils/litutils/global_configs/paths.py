8588238b6356b2f1c0276f8b4bdeba8e20995188
Jan Duchscherer <duchsche@hm.edu>
2024-10-26 16:45:57 +0200

    fix: root path handling in PathConfig

--- src/litutils/litutils/global_configs/paths.py
+++ src/litutils/litutils/global_configs/paths.py
@@ -5,35 +5,34 @@ from pydantic import Field, ValidationInfo, field_validator
 
 from ..utils import BaseConfig
 
-ROOT = Path(__file__).parents[3].resolve()
-
 
 class PathConfig(BaseConfig):
+    root: Path = Field(default_factory=lambda: Path(__file__).parents[4].resolve())
     target: Type["PathConfig"] = Field(default_factory=lambda: PathConfig)
 
-    root: Path = Field(default=ROOT)
     data: Annotated[Path, Field(default=".data")]
     checkpoints: Annotated[Path, Field(default=".logs/checkpoints")]
     tb_logs: Annotated[Path, Field(default=".logs/tb_logs")]
     configs: Annotated[Path, Field(default=".configs")]
     mlflow_uri: Annotated[str, Field(default=".logs/mlflow_logs/mlflow")]
 
-    @field_validator("data", "checkpoints", "tb_logs", "configs")
+    @field_validator("data", "checkpoints", "tb_logs", "configs", mode="before")
     @classmethod
     def __convert_to_path(cls, v: str, info: ValidationInfo) -> Path:
-        root: Path = info.data.get("root")
+        root = info.data.get("root")
         path = (root / v).resolve() if not Path(v).is_absolute() else Path(v)
+        assert isinstance(path, Path)
         path.mkdir(parents=True, exist_ok=True)
-
         return path
 
-    @field_validator("mlflow_uri")
+    @field_validator("mlflow_uri", mode="before")
     @classmethod
     def __convert_to_uri(cls, v: str, info: ValidationInfo) -> str:
+        root = info.data.get("root")
         if v.startswith("file://"):
             return v
-        root: Path = info.data.get("root")
         uri_path = root / v if not Path(v).is_absolute() else Path(v)
+        assert isinstance(uri_path, Path)
         uri_path.parent.mkdir(parents=True, exist_ok=True)
         if not uri_path.exists():
             uri_path.mkdir(parents=True, exist_ok=True)
