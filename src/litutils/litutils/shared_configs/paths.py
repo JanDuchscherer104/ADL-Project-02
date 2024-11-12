0745383d2bf2886335699f4afdff1c4da327ce3e
Jan Duchscherer <duchsche@hm.edu>
2024-11-12 08:48:10 +0100

    feat: minor improvements in paths and wandb configs

--- src/litutils/litutils/shared_configs/paths.py
+++ src/litutils/litutils/shared_configs/paths.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Annotated, Type
 
-from pydantic import Field, ValidationInfo, field_validator, model_validator
+from pydantic import Field, ValidationInfo, field_validator
 
 from ..utils import SingletonConfig
 
@@ -13,33 +14,26 @@ class PathConfig(SingletonConfig):
     data: Annotated[Path, Field(default=".data")]
     webcam_capture: Annotated[Path, Field(default=".data/webcam_capture")]
     checkpoints: Annotated[Path, Field(default=".logs/checkpoints")]
-    tb_logs: Annotated[Path, Field(default=".logs/tb_logs")]
     configs: Annotated[Path, Field(default=".configs")]
     wandb: Annotated[Path, Field(default=".logs/wandb")]
+    optuna_study_uri: Annotated[str, Field(default=".logs/optuna/{study_name}.db")]
 
-    # @field_validator(
-    #     "data",
-    #     "webcam_capture",
-    #     "checkpoints",
-    #     "tb_logs",
-    #     "configs",
-    #     "wandb",
-    #     mode="before",
-    # )
-    # @classmethod
-    # def __convert_to_path(cls, v: str, info: ValidationInfo) -> Path:
-    #     root = info.data.get("root")
-    #     path = (root / v).resolve() if not Path(v).is_absolute() else Path(v)
-    #     assert isinstance(path, Path)
-    #     path.mkdir(parents=True, exist_ok=True)
-    #     return path
+    # AutoDistill related paths
+    autodistill_dataset: Path = Field(
+        default_factory=lambda: Path(".data/combined_groceries")
+    )
+    autodistill_output: Path = Field(
+        default_factory=lambda: Path(".data/output/labels")
+    )
+
     @field_validator(
         "data",
         "webcam_capture",
         "checkpoints",
-        "tb_logs",
         "configs",
         "wandb",
+        "autodistill_dataset",
+        "autodistill_output",
         mode="before",
     )
     @classmethod
@@ -51,15 +45,9 @@ class PathConfig(SingletonConfig):
         v.mkdir(parents=True, exist_ok=True)
         return v
 
-    # @field_validator("mlflow_uri", mode="before")
-    # @classmethod
-    # def __convert_to_uri(cls, v: str, info: ValidationInfo) -> str:
-    #     root = info.data.get("root")
-    #     if v.startswith("file://"):
-    #         return v
-    #     uri_path = root / v if not Path(v).is_absolute() else Path(v)
-    #     assert isinstance(uri_path, Path)
-    #     uri_path.parent.mkdir(parents=True, exist_ok=True)
-    #     if not uri_path.exists():
-    #         uri_path.mkdir(parents=True, exist_ok=True)
-    #     return uri_path.resolve().as_uri()
+    @field_validator("optuna_study_uri", mode="before")
+    @classmethod
+    def convert_to_uri(cls, v: str, info: ValidationInfo) -> str:
+        study_dir = info.data.get("root", Path.cwd()) / Path(v).parent
+        study_dir.mkdir(parents=True, exist_ok=True)
+        return f"sqlite:///{(study_dir / Path(v).name).as_posix()}"
--- src/litutils/litutils/shared_configs/wandb.py
+++ src/litutils/litutils/shared_configs/wandb.py
@@ -1,53 +1,76 @@
-from typing import Any, Optional, Type, Union
+from pathlib import Path
+from typing import Any, Literal, Optional, Type, Union
 
+import wandb
+import wandb.wandb_run
 from pydantic import Field
 from pytorch_lightning.loggers import WandbLogger
 
 from ..utils import BaseConfig
+from .paths import PathConfig
+
+# from wandb.sdk.lib import RunDisabled
+# from wandb.wandb_run import Run
 
 
 class WandbConfig(BaseConfig):
     # Essential fields directly matching WandbLogger parameters
     name: Optional[str] = Field(None, description="Display name for the run.")
-    save_dir: Optional[str] = Field(None, description="Directory to save wandb logs.")
-    version: Optional[str] = Field(None, description="Version ID for resuming runs.")
-    offline: Optional[bool] = Field(False, description="Run offline mode.")
-    dir: Optional[str] = Field(None, description="Alternative to save_dir.")
-    id: Optional[str] = Field(
-        None, description="Same as version; unique ID for the run."
-    )
-    anonymous: Optional[bool] = Field(False, description="Enable anonymous logging.")
     project: Optional[str] = Field(..., description="Name of the wandb project.")
-    log_model: Optional[Union[str, bool]] = Field(
-        False, description="Log model checkpoints as wandb artifacts."
+    save_dir: Path = Field(
+        default_factory=lambda: PathConfig().wandb,
+        serialization_alias="dir",
+        description="Directory to save wandb logs.",
     )
-    prefix: Optional[str] = Field("", description="Prefix for metric keys.")
-    experiment: Optional[Any] = Field(
-        None, description="Predefined wandb experiment object."
+    version: Optional[str] = Field(
+        None,
+        description="Version ID for resuming runs.",
     )
+    offline: Optional[bool] = Field(
+        False,
+        description="Run offline mode.",
+    )
+    log_model: Union[Literal["all"], bool] = Field(
+        False,
+        description="Log model checkpoints as wandb artifacts.",
+    )
+    prefix: Optional[str] = Field(
+        "",
+        description="Str prefix for beginning of metric keys.",
+    )
+    # experiment: Optional[Union[Run, RunDisabled]] = Field(
+    #     None, description="Predefined wandb run object."
+    # )
     checkpoint_name: Optional[str] = Field(
         None, description="Name of model checkpoint artifact."
     )
+    tags: Optional[list[str]] = Field(
+        None, description="List of tags for easier filtering."
+    )
 
-    target: Type[WandbLogger] = Field(None, validate_default=False)
+    group: Optional[str] = Field(None, description="Group name for multiple runs.")
+    job_type: Optional[str] = Field(None, description="Type of job for the run.")
+    target: Type[WandbLogger] = Field(None, exclude=True, validate_default=False)
 
     def setup_target(self, **kwargs: Any) -> WandbLogger:
         """
-        Initializes the WandbLogger with the specified configurations.
+        Initializes the [WandbLogger](https://lightning.ai/docs/pytorch/stable/extensions/generated/lightning.pytorch.loggers.WandbLogger.html) with the specified configurations.
+
+        For kwargs refer to [wandb.init](https://docs.wandb.ai/ref/python/init/)
         """
         logger = WandbLogger(
             name=self.name,
             save_dir=self.save_dir,
             version=self.version,
             offline=self.offline,
-            dir=self.dir,
-            id=self.id,
-            anonymous=self.anonymous,
             project=self.project,
             log_model=self.log_model,
             prefix=self.prefix,
-            experiment=self.experiment,
+            experiment=wandb.run,
             checkpoint_name=self.checkpoint_name,
+            tags=self.tags,
+            group=self.group,
+            job_type=self.job_type,
             **(kwargs or {}),
         )
 
